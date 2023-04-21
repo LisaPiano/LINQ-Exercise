e91410da5a799b8525f44605a4c4b2f8b03b7c05
Lisa Walters <lisacampiwalters@gmail.com>
2023-04-21 00:56:14 -0600

    fixed the problem and completed project

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,8 +6,8 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            List<string> VideoGames = new List<string>() {"Mario Brothers", "Resident Evil", "Silent Hill", "Pac Man", "Tetrus" };   
-            VideoGames.OrderByDescending(word => word.Length);
+            List<string> VideoGames = new List<string>() {"Mario Brothers", "Resident Evil", "Silent Hill", "Pac Man", "Tetrus", "Harry Potter Video Game"};   
+           VideoGames = VideoGames.OrderByDescending(word => word.Length).ToList();
             
             foreach (var game in VideoGames) 
             {
